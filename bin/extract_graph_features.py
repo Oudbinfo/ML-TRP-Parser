b92c97c73608e22302a30cca63a75ada390e145f
soroushm <soroush.binfo@gmail.com>
2023-08-23 17:16:45 +0200

    extract_graph_features.py was modified to handle exceptional cases

--- bin/extract_graph_features.py
+++ bin/extract_graph_features.py
@@ -9,7 +9,10 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Define the essential inputs
-repeatsdb_df = pd.read_csv("../data/repeatsdb_unique_updated.csv", dtype={'ct': str})
+repeatsdb_df = pd.read_csv("../data/repeatsdb_supplemented_updated.csv", dtype={"ct": str})
+# Create a list of region ids for the PDBs that contain more than one region with same topology (ct)
+double_region_list = repeatsdb_df.groupby(['pdb', 'ct']).filter(lambda group: len(group) > 1)["region_id"].tolist()
+
 in_dir = "/home/soroushm/Documents/ML-TRP-Parser_workframe/dataframes/graph_tmscore_output"
 out_dir = "/home/soroushm/Documents/ML-TRP-Parser_workframe/dataframes/graph_features"
 
@@ -28,21 +31,29 @@ for file_path in file_path_list:
         graph_df_path = os.path.join(in_dir, f"{region_id[1:3]}", f"{region_id}_vs_{region_id}_graph.csv")
         graph_df = pd.read_csv(graph_df_path)
 
+        # If region_id in double_region_df, limit the boundaries
+        if region_id in double_region_list:
+            region_start = int(region_id.split("_")[1])
+            region_end = int(region_id.split("_")[2])
+            graph_df = graph_df[region_start <= graph_df["x_start"]]
+            graph_df = graph_df[region_end >= graph_df["x_start"]]
+
         # Extract x, y and length from the graph dataframe
         # Convert into numpy arrays
         x = np.array(graph_df["x_start"])
         y = np.array(graph_df["y"])
-        length = graph_df.loc[0, "x_end"] - graph_df.loc[0, "x_start"] + 1  # Last residue is inclusive
+        window_len = graph_df.iloc[0, 1] - graph_df.iloc[0, 0] + 1 # Last residue is inclusive
 
         # Extract the data regarding the annotated units and their starting point
         units = repeatsdb_df[repeatsdb_df["region_id"] == region_id]["units"].values[0].split(",")
         unit_start_list = [int(i.split("_")[0]) for i in units]
 
         ct = str(repeatsdb_df[repeatsdb_df["region_id"] == region_id]["ct"].values[0])
+        window_avg = repeatsdb_df[repeatsdb_df["region_id"] == region_id]["units_avg"].values[0]
 
         # Smooth the graph by n residue window size
         # (small window size --> reducing noise / not compromising resolution)
-        window_size = round(length * 0.2)
+        window_size = round(window_len * 0.2)
         if window_size % 2 == 0:
             window_size -= 1
         poly_order = 0
@@ -51,7 +62,7 @@ for file_path in file_path_list:
         x, y = adjust_graph_ends(x, y)
 
         # Detect peaks and associated properties
-        peaks, properties = find_peaks(y, height=0.2, distance=round(length * 0.5),
+        peaks, properties = find_peaks(y, height=0.2, distance=round(window_len * 0.5),
                                        threshold=0, prominence=0, width=0, plateau_size=0)
 
         # Mark the peaks on y and x-axis
@@ -60,9 +71,13 @@ for file_path in file_path_list:
 
         peaks_df = pd.DataFrame(properties)
         peaks_df["peak_x"] = peaks_x
-        column_list = ["left_edges", "right_edges", "left_ips", "right_ips", "left_bases", "right_bases"]
+
+        column_list = ["left_edges", "right_edges", "left_ips", "right_ips", "left_bases", "right_bases",]
+
         for column in column_list:
-            peaks_df[column] = (peaks_df['peak_x'] - peaks_df[column]) / length
+            peaks_df[column] = (peaks_df['peak_x'] - peaks_df[column]) / window_len
+
+        peaks_df["widths"] = peaks_df["widths"] / window_len
 
         # Create shifted dataframes
         peaks_df_previous1 = peaks_df.shift(1)
@@ -71,24 +86,21 @@ for file_path in file_path_list:
         # Rename columns
         peaks_df_previous1.columns = ['pre1_' + col for col in peaks_df.columns]
         peaks_df_next1.columns = ['post1_' + col for col in peaks_df.columns]
-
         # Concatenate
         peaks_df = pd.concat([peaks_df_previous1, peaks_df, peaks_df_next1], axis=1)
         peaks_df.fillna(0, inplace=True)
 
         unit_start_range_list = []
-        margin = round(length * 0.2)
+        margin = round(window_len * 0.2)
         for unit_start in unit_start_list:
             range_start = unit_start - margin
             range_end = unit_start + margin
             unit_start_range = (range_start, range_end)
             unit_start_range_list.append(unit_start_range)
 
-
         def is_in_range(number, range_tuple):
             start, end = range_tuple
             return start <= number <= end
-
 
         peak_labels = []
         for x_start in peaks_x:
@@ -98,12 +110,13 @@ for file_path in file_path_list:
                     truth = 1
             peak_labels.append(truth)
 
-        peaks_df["peak_x"] = peaks_x
         peaks_df["ct"] = [ct for i in range(len(peaks_df))]
-        peaks_df["window_len"] = [length for i in range(len(peaks_df))]
+        peaks_df["window_avg"] = [window_avg for i in range(len(peaks_df))]
+        peaks_df["window_len"] = [window_len for i in range(len(peaks_df))]
         peaks_df["label"] = peak_labels
 
-        peaks_df.drop(["pre1_peak_x", "peak_x", "post1_peak_x"], axis=1, inplace=True)
+        peaks_df.drop(["pre1_peak_x", "peak_x", "post1_peak_x", "pre1_plateau_sizes",
+                       "post1_plateau_sizes", "plateau_sizes"], axis=1, inplace=True)
 
         out_subdir = os.path.join(out_dir, region_id[1:3])
         os.makedirs(out_subdir, exist_ok=True)
